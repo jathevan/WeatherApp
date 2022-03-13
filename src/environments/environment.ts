17af19abaac9df71fc7c26a8bce9d627b886767f
Jathevan <jathevan15@gmail.com>
2022-03-13 23:45:59 +1000

    Change the configuration for Heroku deployment.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   apiKey: '8ad708f25a87fc727463a622639f13bf',
   apiUrl: 'http://api.openweathermap.org/data/2.5'
 };
